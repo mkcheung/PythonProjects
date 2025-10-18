efb5abbd7e56fa356a361ccb3a06574ba0d4ba7b
Mars Cheung <mars.kwong.cheung@gmail.com>
2025-10-17 21:40:57 -0700

    complete hangman game

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -9,9 +9,14 @@
 # e.g. If the user guessed "p" and the chosen word was "apple", then `display` should be `_ p p _ _`.
 # Print `display` and you should see the guessed letter in the correct position.
 # But every letter that is not a match is represented with a "_".
+
+# Use a while loop to let the user guess again. 
+# The loop should only stop once the user has guessed all the letters in the chosen_word.
+# At that point `display` has no more blanks ("_"). Then you can tell the user they've won.
 import random
 
 placeholder = ''
+game_over = False
 
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list).lower()
@@ -22,14 +27,24 @@ for i in range(0, numChars):
 
 print(placeholder)
 
-display = ''
+correctLetters = []
 
-guess = input(f"Please guess a letter.\n")
+while not game_over:
+    guess = input(f"Please guess a letter.\n").lower()
 
-for char in chosen_word:
-    if(char == guess):
-        display += char
-    else:
-        display += '_'
+    display = ''
 
-print(display)
+    for char in chosen_word:
+        if(char == guess):
+            display += char
+            correctLetters.append(char)
+        elif(char in correctLetters):
+            display += char
+        else:
+            display += '_'
+
+    print(display)
+
+    if( '_' not in display):
+        game_over = True
+        print("You win.")
