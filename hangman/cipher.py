3b3ca90049a807f5cb01b05229a2eba4f10e2572
Mars Cheung <mars.kwong.cheung@gmail.com>
2025-10-18 16:05:06 -0700

    add art to cipher program and shutdown feature

--- hangman/cipher.py
+++ hangman/cipher.py
@@ -1,35 +1,52 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+import art
 
-direction = input("Type 'encode' to encrypt. Type 'decode' to decrypt.\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+print(art.logo)
 
-def encrypt(original_text, shift_amount):
-    cipherTerm = ''
-    for char in original_text:
-        indexOfChar = alphabet.index(char)
-        indexOfNewChar = indexOfChar + shift_amount
-        indexOfNewChar %= len(alphabet)
-        cipherTerm += alphabet[indexOfNewChar]
-    print(f"{cipherTerm}\n")
+active = True
 
-def decrypt(original_text, shift_amount):
-    decryptedTerm = ''
-    shift_amount *= -1
-    for char in original_text:
-        originalCharIndex = alphabet.index(char)
-        newCharIndex = originalCharIndex + shift_amount
-        newCharIndex %= len(alphabet)
-        decryptedTerm += alphabet[newCharIndex]
-    print(f"{decryptedTerm}\n")
+while active:
+    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+    direction = input("Type 'encode' to encrypt. Type 'decode' to decrypt.\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    def encrypt(original_text, shift_amount):
+        cipherTerm = ''
+        for char in original_text:
+            if char not in alphabet:
+                cipherTerm += char
+                continue
+            indexOfChar = alphabet.index(char)
+            indexOfNewChar = indexOfChar + shift_amount
+            indexOfNewChar %= len(alphabet)
+            cipherTerm += alphabet[indexOfNewChar]
+        print(f"{cipherTerm}\n")
+
+    def decrypt(original_text, shift_amount):
+        decryptedTerm = ''
+        shift_amount *= -1
+        for char in original_text:
+            if char not in alphabet:
+                decryptedTerm += char
+                continue
+            originalCharIndex = alphabet.index(char)
+            newCharIndex = originalCharIndex + shift_amount
+            newCharIndex %= len(alphabet)
+            decryptedTerm += alphabet[newCharIndex]
+        print(f"{decryptedTerm}\n")
 
 
-def caesar(direction, text, shift):
-    if(direction == 'encode'):
-        encrypt(original_text=text, shift_amount=shift)
-    elif(direction == 'decode'):
-        decrypt(original_text=text, shift_amount=shift)
-    else:
-        print("Invalid Selection")
+    def caesar(direction, text, shift):
+        if(direction == 'encode'):
+            encrypt(original_text=text, shift_amount=shift)
+        elif(direction == 'decode'):
+            decrypt(original_text=text, shift_amount=shift)
+        else:
+            print("Invalid Selection")
 
-caesar(direction=direction, text=text, shift=shift)
+    caesar(direction=direction, text=text, shift=shift)
+    stillActive = input("Do you want to try again?\nPlease enter True or False\n")
+    if stillActive == 'False':
+        active = False
+        print('Goodbye')
