692ad74743293cd9bcbfa93c7619994e219d8bc6
Mars Cheung <mars.kwong.cheung@gmail.com>
2025-10-17 21:09:37 -0700

    begin hangman game code

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,18 +1,35 @@
-# TODO-1 Randomly choose a word from the word_list and assign it to a variable called chosen_word. Then print it.
-# TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-# TODO-3 Check if the letter the user guessed (guess) is one of the letters in the chosen_word. Print "Right" if it is, "wrong" if it's not
+# Create an empty String called `placeholder`.
+# For each letter in the chosen_word, add a `_` to `placeholder`.
+# So if the `chosen_word` was "apple", `placeholder` should be `_ _ _ _ _` with 5 `"_"` representing each letter to guess.
+# Print out `hint`.
 
+# Create an empty string called "display".
+# Loop through each letter in the `chosen_word`
+# If the letter at that position matches `guess` then reveal that letter in the `display` at that position.
+# e.g. If the user guessed "p" and the chosen word was "apple", then `display` should be `_ p p _ _`.
+# Print `display` and you should see the guessed letter in the correct position.
+# But every letter that is not a match is represented with a "_".
 import random
 
-word_list = ["aardvark", "baboon", "camel"]
+placeholder = ''
 
+word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list).lower()
 
-print(chosen_word)
+numChars = len(chosen_word)
+for i in range(0, numChars):
+    placeholder += "_"
+
+print(placeholder)
+
+display = ''
 
 guess = input(f"Please guess a letter.\n")
 
-if guess in chosen_word:
-    print("Right")
-else:
-    print("wrong")
+for char in chosen_word:
+    if(char == guess):
+        display += char
+    else:
+        display += '_'
+
+print(display)
