1cb3550b7ac203d4effaf790927a19e29770d549
Mars Cheung <mars.kwong.cheung@gmail.com>
2025-10-18 23:10:31 -0700

    add blackjack game

---
+++ blackjack/blackjack.py
@@ -0,0 +1,50 @@
+import random
+def deal_card():
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    return random.choice(cards)
+
+playerDealtCards = []
+computerDealt = []
+computerTotal = 0
+activeHits = True;
+
+for num in range(0, 2):
+    playerDealtCards.append(deal_card())
+    computerDealt.append(deal_card())
+
+print(f"You have cards {playerDealtCards[0]} {playerDealtCards[1]}")
+print(f"Computer shows one card: {computerDealt[0]}")
+
+while activeHits:
+    playerTotal = 0
+    newCard = input("Hit? Please enter 'y' or 'n': ")
+    playerCardsAndTotal = ''
+
+    if newCard == 'y':
+        cards = ''
+        playerDealtCards.append(deal_card())
+        for num in playerDealtCards:
+            cards +=  f"{num}, "
+
+    for num in playerDealtCards:
+        playerTotal += num  
+
+    print(f"Player cards: {cards} - Total: {playerTotal}")
+
+    if playerTotal > 21 or newCard == 'n':
+        activeHits = False
+    
+for num in computerDealt:
+    computerTotal += num
+
+if playerTotal > 21 or computerTotal <= 21 and playerTotal < computerTotal:
+    print(f"Player {playerTotal} : Computer {computerTotal} - You Lose!")
+elif playerTotal > computerTotal:
+    print(f"Player {playerTotal} : Computer {computerTotal} - You Win!")
+elif playerTotal == computerTotal:
+    print(f"Player {playerTotal} : Computer {computerTotal} - Draw")
+
+
+
+
+
