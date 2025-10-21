03522c11e41e5c6ed06ede528e327e874127d6af
Mars Cheung <mars.kwong.cheung@gmail.com>
2025-10-20 21:05:31 -0700

    add simple square project utilizing turtle

---
+++ turtle/turtleProject.py
@@ -0,0 +1,15 @@
+from turtle import Turtle, Screen
+
+t = Turtle()
+
+t.color("red")
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+
+screen = Screen()
+screen.exitonclick()
