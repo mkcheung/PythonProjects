867372150d2a9d447bfdbe15b80a92125ae27113
Mars Cheung <mars.kwong.cheung@gmail.com>
2025-10-20 22:00:17 -0700

    adjust colormode to set by tuple

--- turtle/turtleRandomWalk.py
+++ turtle/turtleRandomWalk.py
@@ -1,13 +1,23 @@
-from turtle import Turtle, Screen
+from turtle import Turtle, Screen, colormode
 import random
 
+colormode(255)
 t = Turtle()
-colors = ["red", "CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
-headings = (90, 180, 270, 360)
+# colors = ["red", "CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
+headings = [90, 180, 270, 360]
 distance = 100
+penSizes = [40, 52, 23, 10]
+
+def randomColor():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return (r, g, b)
 
 for i in range(1, 20):
-    t.color(random.choice(colors))
+    # t.color(random.choice(colors))
+    t.pensize(random.choice(penSizes))
+    t.color(randomColor())
     t.forward(distance)
     t.setheading(random.choice(headings))
 
