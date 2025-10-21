eb1ce76fc1d312d07d4ce07a0cff4ba3d0581f46
Mars Cheung <mars.kwong.cheung@gmail.com>
2025-10-21 15:50:57 -0700

    adjust snake movement so it cannot eat itself

--- snake/snakeModel.py
+++ snake/snakeModel.py
@@ -1,4 +1,9 @@
 from turtle import Turtle, Screen
+DISTANCE = 20
+RIGHT = 0
+DOWN = 90
+LEFT = 180
+UP = 270
 
 class Snake:
     def __init__(self, startingLocations):
@@ -16,4 +21,21 @@ class Snake:
             newY = self.turtleSegments[tSegNum-1].ycor()
             self.turtleSegments[tSegNum].goto(newX, newY)
         
-        self.turtleSegments[0].forward(20)
+        self.turtleSegments[0].forward(DISTANCE)
+
+    
+    def right(self):
+        if self.turtleSegments[0].heading() != RIGHT and self.turtleSegments[0].heading() != LEFT:
+            self.turtleSegments[0].setheading(RIGHT)
+
+    def up(self):
+        if self.turtleSegments[0].heading() != DOWN and self.turtleSegments[0].heading() != UP:
+            self.turtleSegments[0].setheading(DOWN)
+
+    def left(self):
+        if self.turtleSegments[0].heading() != LEFT and self.turtleSegments[0].heading() != RIGHT:
+            self.turtleSegments[0].setheading(LEFT)
+
+    def down(self):
+        if self.turtleSegments[0].heading() != UP and self.turtleSegments[0].heading() != DOWN:
+            self.turtleSegments[0].setheading(UP)
--- snake/snake_game.py
+++ snake/snake_game.py
@@ -4,6 +4,7 @@ from snakeModel import Snake
 
 game_active = True
 startingLocations = [(0,0), (-20,0), (-40,0), (-60,0)]
+
 snake = Snake(startingLocations)
 
 screen = Screen()
@@ -11,6 +12,12 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer(0)
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.right, "Right")
 
 while game_active:
     screen.update() 
