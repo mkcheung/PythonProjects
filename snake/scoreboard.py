8a7873ce40f9036fbba742d54f90d944f3744986
Mars Cheung <mars.kwong.cheung@gmail.com>
2025-10-27 15:25:16 -0700

    add read and write of high scores to file

--- snake/scoreboard.py
+++ snake/scoreboard.py
@@ -1,10 +1,15 @@
+import os
 from turtle import Turtle
 
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
+        if os.path.exists("./high_score.txt"):
+            with open("./high_score.txt", "r") as file:
+                self.high_score = int(file.read());
+        else:
+            self.high_score = 0
         self.score = 0
-        self.high_score = 0
         self.penup()
         self.color('white')
         self.goto(0,260)
@@ -18,6 +23,8 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("./high_score.txt", "w") as file:
+                file.write(f"{self.high_score}")
         self.score = 0
         self.update_scoreboard()
 
