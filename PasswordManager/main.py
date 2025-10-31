0739902caba7ad282eb566f99e01f86c7d1f8758
Mars Cheung <mars.kwong.cheung@gmail.com>
2025-10-31 14:56:26 -0700

    add pyperclip module and save new passwords to clipboard

--- PasswordManager/main.py
+++ PasswordManager/main.py
@@ -1,7 +1,25 @@
 import os
 from pathlib import Path
+import pyperclip
+from random import choice, randint, shuffle
 from tkinter import *
 from tkinter import messagebox
+def generatePassword():
+    global PasswordInput
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_letters = [ choice(letters) for _ in range(randint(8, 10))]
+    password_symbols = [ choice(symbols) for _ in range(randint(2, 4))]
+    password_numbers = [ choice(numbers) for _ in range(randint(2, 4))]
+
+    password_list = password_letters + password_symbols + password_numbers
+    shuffle(password_list)
+    password = "".join(password_list)
+    PasswordInput.insert(0, string=password)
+    pyperclip.copy(password)
+
 
 FONT_NAME = "Courier"
 
@@ -34,7 +52,7 @@ Password.grid(row=3, column=0)
 PasswordInput = Entry(width=21, bg=WHITE)
 PasswordInput.grid(row=3,column=1)
 
-generatePasswordButton = Button(text="Generate Password", bg=WHITE);
+generatePasswordButton = Button(text="Generate Password", bg=WHITE, command=generatePassword);
 generatePasswordButton.grid(row=3,column=2)
 
 def writeToFile():
