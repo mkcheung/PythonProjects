955853ba24c65fafca9cd3301d540957410c6e87
Mars Cheung <mars.kwong.cheung@gmail.com>
2025-10-31 11:24:16 -0700

    adjust design

--- PasswordManager/main.py
+++ PasswordManager/main.py
@@ -15,24 +15,24 @@ padlock_img = PhotoImage(file=f"{workingDirectory}/logo.png")
 canvas.create_image(100, 100, image=padlock_img)
 canvas.grid(row=0, column=1)
 
-websiteLabel = Label(text="Website:", justify="center", anchor="center", bg=WHITE, font=(FONT_NAME,10))
+websiteLabel = Label(text="Website:", bg=WHITE)
 websiteLabel.grid(row=1, column=0)
 websiteInput = Entry(width=35)
-websiteInput.grid(row=1, column=1)
+websiteInput.grid(row=1, column=1, columnspan=2)
 
-EmailUsernameLabel = Label(text="Email/Username:", justify="center", anchor="center", bg=WHITE, font=(FONT_NAME,10))
+EmailUsernameLabel = Label(text="Email/Username:", bg=WHITE)
 EmailUsernameLabel.grid(row=2, column=0)
 EmailUsernameInput = Entry(width=35, bg=WHITE)
-EmailUsernameInput.grid(row=2, column=1)
+EmailUsernameInput.grid(row=2, column=1, columnspan=2)
 
-Password = Label(text="Password:", justify="center", anchor="center", bg=WHITE, font=(FONT_NAME,10))
+Password = Label(text="Password:", bg=WHITE)
 Password.grid(row=3, column=0)
 PasswordInput = Entry(width=21, bg=WHITE)
 PasswordInput.grid(row=3,column=1)
 
 generatePasswordButton = Button(text="Generate Password", bg=WHITE);
 generatePasswordButton.grid(row=3,column=2)
-addButton = Radiobutton(text="Add", bg=WHITE)
-addButton.grid(row=4, column=1)
+addButton = Button(text="Add",width=36)
+addButton.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
