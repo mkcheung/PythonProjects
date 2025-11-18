db3a400ac5a6136648869fd1872533e49ead07d0
Mars Cheung <mars.kwong.cheung@gmail.com>
2025-11-17 19:05:48 -0800

    add search frequency vs price for bitcoin

--- googleTrends/server.py
+++ googleTrends/server.py
@@ -5,28 +5,33 @@ from pathlib import Path
 import pandas as pd
 
 
+# Tesla And Charts
 workingDirectory = Path(__file__).resolve().parent;
-teslaDf = pd.read_csv(f"{workingDirectory}/TESLA Search Trend vs Price.csv")
-teslaDf.MONTH = pd.to_datetime(teslaDf.MONTH)
-print(teslaDf)
-print(teslaDf.shape)
-print(teslaDf.columns)
-print(f"Max Search Tesla: {teslaDf['TSLA_WEB_SEARCH'].max()}")
-print(f"Min Search Tesla: {teslaDf['TSLA_WEB_SEARCH'].min()}")
-print(f"Missing Values for Tesla DF: {teslaDf.isna().values.any()}")
+# teslaDf = pd.read_csv(f"{workingDirectory}/TESLA Search Trend vs Price.csv")
+# teslaDf.MONTH = pd.to_datetime(teslaDf.MONTH)
+# print(teslaDf)
+# print(teslaDf.shape)
+# print(teslaDf.columns)
+# print(f"Max Search Tesla: {teslaDf['TSLA_WEB_SEARCH'].max()}")
+# print(f"Min Search Tesla: {teslaDf['TSLA_WEB_SEARCH'].min()}")
+# print(f"Missing Values for Tesla DF: {teslaDf.isna().values.any()}")
 
-plt.figure(figsize=(14,8), dpi=120) 
-plt.title("Tesla Web Search vs Price")
-plt.xticks(fontsize=14, rotation=45)
-plt.yticks(fontsize=14)
-ax1 = plt.gca()
-ax2 = ax1.twinx()
-ax1.set_xlabel('Year')
-ax1.set_ylabel('Search Trend', color='skyblue')
-ax2.set_ylabel('Stock Price', color='#E6232E')
-ax1.plot(teslaDf.MONTH, teslaDf.TSLA_WEB_SEARCH, color='skyblue')
-ax2.plot(teslaDf.MONTH, teslaDf.TSLA_USD_CLOSE, color='#E6232E')
-plt.show()
+# plt.figure(figsize=(14,8), dpi=120) 
+# plt.title("Tesla Web Search vs Price")
+# plt.xticks(fontsize=14, rotation=45)
+# plt.yticks(fontsize=14)
+# ax1 = plt.gca()
+# ax2 = ax1.twinx()
+# ax1.set_xlabel('Year')
+# ax1.set_ylabel('Search Trend', color='skyblue')
+# ax2.set_ylabel('Stock Price', color='#E6232E')
+# ax1.plot(teslaDf.MONTH, teslaDf.TSLA_WEB_SEARCH, color='skyblue')
+# ax2.plot(teslaDf.MONTH, teslaDf.TSLA_USD_CLOSE, color='#E6232E')
+# plt.show()
+
+
+
+
 
 # ueBenefits20042020Df = pd.read_csv(f"{workingDirectory}/UE Benefits Search vs UE Rate 2004-20.csv")
 # ueBenefits20042020Df.MONTH = pd.to_datetime(ueBenefits20042020Df.MONTH)
@@ -46,8 +51,11 @@ plt.show()
 # print(f"UE Benefits 2004 - 2019 Min Search Tesla: {ueBenefits20042019Df['UE_BENEFITS_WEB_SEARCH'].min()}")
 # print(f"Missing Values for UE Benefits 2004 - 2019: {ueBenefits20042019Df.isna().values.any()}")
 
-# bitcoinDf = pd.read_csv(f"{workingDirectory}/Bitcoin Search Trend.csv")
-# bitcoinDf.MONTH = pd.to_datetime(bitcoinDf.MONTH)
+
+
+# Bitcoin
+bitcoinDf = pd.read_csv(f"{workingDirectory}/Bitcoin Search Trend.csv")
+bitcoinDf.MONTH = pd.to_datetime(bitcoinDf.MONTH)
 # print(bitcoinDf)
 # print(bitcoinDf.shape)
 # print(bitcoinDf.columns)
@@ -55,12 +63,13 @@ plt.show()
 # print(f"Bitcoin Search Trend Min: {bitcoinDf['BTC_NEWS_SEARCH'].min()}")
 # print(f"Missing Values for Bitcoin: {bitcoinDf.isna().values.any()}")
 
-# dailybitcoinDf = pd.read_csv(f"{workingDirectory}/Daily Bitcoin Price.csv")
-# dailybitcoinDf.DATE = pd.to_datetime(dailybitcoinDf.DATE)
+dailybitcoinDf = pd.read_csv(f"{workingDirectory}/Daily Bitcoin Price.csv")
+dailybitcoinDf.DATE = pd.to_datetime(dailybitcoinDf.DATE)
 # print(dailybitcoinDf)
 # print(dailybitcoinDf.shape)
 # print(dailybitcoinDf.columns)
-# np_dailybitCoin = dailybitcoinDf.isna().values;
+np_dailybitCoin = dailybitcoinDf.isna().values;
+
 
 # # #find out where in the dataframe a value resides
 # (rows, cols) = np.where(np_dailybitCoin==True)
@@ -74,5 +83,18 @@ plt.show()
 # dailybitcoinDf.dropna(inplace=True)
 
 # # resampled
-# monthlybitcoinDf = dailybitcoinDf.resample('M', on='DATE').last()
-# print(monthlybitcoinDf)
+monthlybitcoinDf = dailybitcoinDf.resample('M', on='DATE').last()
+print(monthlybitcoinDf)
+# Bitcoin And Charts - Price vs Search 
+plt.figure(figsize=(14,8), dpi=120) 
+plt.title("Bitcoin News Search vs Resampled Price")
+plt.xticks(fontsize=14, rotation=45)
+plt.yticks(fontsize=14)
+ax1 = plt.gca()
+ax2 = ax1.twinx()
+ax1.set_xlabel('Year')
+ax1.set_ylabel('Search Trend', color='skyblue')
+ax2.set_ylabel('BTC Price', color='#E6232E')
+ax1.plot(bitcoinDf.MONTH, bitcoinDf.BTC_NEWS_SEARCH, color='skyblue')
+ax2.plot(monthlybitcoinDf.index, monthlybitcoinDf.CLOSE, color='#E6232E')
+plt.show()
