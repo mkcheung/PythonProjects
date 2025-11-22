bc52548a2a7abf3ff935b840a6c70a45d77fe86a
Mars Cheung <mars.kwong.cheung@gmail.com>
2025-11-21 17:39:27 -0800

    establish chlorpelth and sunburst graphs

--- nobelPrizeAnalysis/server.py
+++ nobelPrizeAnalysis/server.py
@@ -1,9 +1,12 @@
+import json
 import matplotlib.pyplot as plt
 import numpy as np
 import os
 from pathlib import Path
 import pandas as pd
 import plotly.express as px
+from urllib.request import urlopen
+
 
 workingDirectory = Path(__file__).resolve().parent
 nobelPrizeDf = pd.read_csv(f"{workingDirectory}/nobel_prize_data.csv")
@@ -109,38 +112,155 @@ print(movingAvgPricesPerYear)
 yearlyAvgShare = nobelPrizeDf.groupby(by="year").agg({'share_pct':'mean'})
 shareMovingAvg = yearlyAvgShare.rolling(window=5).mean()
 
-plt.figure(figsize=(16,8), dpi=200)
-plt.title('Number of Nobel Prizes Awarded per Year', fontsize=18)
+# plt.figure(figsize=(16,8), dpi=200)
+# plt.title('Number of Nobel Prizes Awarded per Year', fontsize=18)
+# plt.yticks(fontsize=14)
+# plt.xticks(ticks=np.arange(1900, 2021, step=5), 
+#     fontsize=14, 
+#     rotation=45
+# )
+
+# ax1 = plt.gca()
+# ax1 = ax1.twinx()
+# ax2 = ax1.twinx() 
+
+# ax1.set_xlim(1900, 2020)
+# ax2.invert_yaxis()
+ 
+# ax1.scatter(x=prizesPerYear.index, 
+#     y=prizesPerYear.values, 
+#     c='dodgerblue',
+#     alpha=0.7,
+#     s=100
+# )
+
+# ax1.plot(prizesPerYear.index, 
+#     movingAvgPricesPerYear.values, 
+#     c='crimson', 
+#     linewidth=3
+# )
+
+# ax2.plot(prizesPerYear.index, 
+#     shareMovingAvg.values, 
+#     c='grey', 
+#     linewidth=3
+# )
+ 
+# plt.show()
+
+# Top 20 Country Ranking
+print(nobelPrizeDf)
+top20_countries = nobelPrizeDf.groupby('birth_country_current').agg({"prize":"count"})
+top20_countries.sort_values('prize', ascending=True, inplace=True)
+top20_countries = top20_countries[-20:]
+
+# plt.figure(figsize=(14,8))
+# plt.xticks(fontsize=14, rotation=45)
+# plt.yticks(fontsize=14)
+# plt.ylabel('Prizes Awarded', fontsize=14)
+# plt.xlabel('Countries', fontsize=14)
+# print(top20_countries.index)
+# print(top20_countries.values)
+# plt.barh(top20_countries.index, top20_countries.values.flatten())
+# plt.show()
+
+top20_countriesISO = nobelPrizeDf.groupby(['birth_country_current', 'ISO'], as_index=False
+).agg({'prize':'count'})
+print(top20_countriesISO)
+top20_countriesISO.sort_values('prize', ascending=False)
+worldMap = px.choropleth(
+    top20_countriesISO, 
+    locations="ISO",
+    color="prize",
+    hover_name="birth_country_current", 
+    color_continuous_scale=px.colors.sequential.matter
+)
+# worldMap.update_layout(coloraxis_showscale=True,)
+# worldMap.show()
+
+#category breakdown by country
+top20Count_byCategory = nobelPrizeDf.groupby(['birth_country_current', 'category'], as_index=False).agg({'prize':'count'})
+top20Count_byCategory.sort_values('prize', ascending=False, inplace=True)
+print(top20Count_byCategory)
+
+mergedDf = pd.merge(top20Count_byCategory, top20_countriesISO, on='birth_country_current')
+print(mergedDf)
+# rename the columns to make it easier to read
+# mergedDf.columns = ['birth_country_current', 'category', 'cat_prize', 'ISO', 'total_prize']
+# mergedDf.sort_values(by='total_prize', inplace=True)
+# print(mergedDf)
+
+# categoryCountryBar = px.bar (
+#     x=mergedDf['cat_prize'],
+#     y=mergedDf['birth_country_current'],
+#     color=mergedDf['category'],
+#     orientation='h',
+#     title='Top 20 Countries by Number of Prizes and Category'
+# )
+# categoryCountryBar.update_layout(xaxis_title='Number of Prizes', 
+#                             yaxis_title='Country')
+# categoryCountryBar.show()
+
+
+# Research Locations
+groupedByResearchLocs = nobelPrizeDf.groupby('organization_name').agg({"prize":"count"})
+groupedByResearchLocs.sort_values('prize', ascending=True, inplace=True)
+top20 = groupedByResearchLocs[-20:]
+
+plt.figure(figsize=(14,8))
+plt.xticks(fontsize=14, rotation=45)
 plt.yticks(fontsize=14)
-plt.xticks(ticks=np.arange(1900, 2021, step=5), 
-    fontsize=14, 
-    rotation=45
-)
-
-ax1 = plt.gca()
-ax1 = ax1.twinx()
-ax2 = ax1.twinx() 
-
-ax1.set_xlim(1900, 2020)
-ax2.invert_yaxis()
- 
-ax1.scatter(x=prizesPerYear.index, 
-    y=prizesPerYear.values, 
-    c='dodgerblue',
-    alpha=0.7,
-    s=100
-)
-
-ax1.plot(prizesPerYear.index, 
-    movingAvgPricesPerYear.values, 
-    c='crimson', 
-    linewidth=3
-)
-
-ax2.plot(prizesPerYear.index, 
-    shareMovingAvg.values, 
-    c='grey', 
-    linewidth=3
-)
- 
+plt.ylabel('Organizations ', fontsize=14)
+plt.xlabel('Number of Prizes', fontsize=14)
+plt.barh(top20.index, top20.values.flatten())
 plt.show()
+
+top20BirthCities = nobelPrizeDf['organization_city'].value_counts()[:20]
+top20BirthCities.sort_values(ascending=True, inplace=True)
+top20BirthCitiesBar = px.bar(x = top20BirthCities.values,
+    y = top20BirthCities.index,
+    orientation='h',
+    color=top20BirthCities.values,
+    color_continuous_scale=px.colors.sequential.Plasma,
+    title='Which Cities Do the Most Research?'
+)
+
+top20BirthCitiesBar.update_layout(xaxis_title='Number of Prizes', 
+                        yaxis_title='City',
+                       coloraxis_showscale=False)
+top20BirthCitiesBar.show()
+
+top20BirthCities = nobelPrizeDf['birth_city'].value_counts()[:20]
+top20BirthCities.sort_values(ascending=True, inplace=True)
+top20BirthCitiesBar = px.bar(x = top20BirthCities.values,
+    y = top20BirthCities.index,
+    orientation='h',
+    color=top20BirthCities.values,
+    color_continuous_scale=px.colors.sequential.Plasma,
+    title='Where were the Nobel Laureates Born?'
+)
+
+top20BirthCitiesBar.update_layout(xaxis_title='Number of Prizes', 
+    yaxis_title='City of Birth',
+    coloraxis_showscale=False
+)
+top20BirthCitiesBar.show()
+
+countryCityOrg = nobelPrizeDf.groupby(['organization_country','organization_city','organization_name'], as_index=False).agg({'prize':'count'})
+countryCityOrg.sort_values('prize', ascending=False)
+print(countryCityOrg)
+
+burst = px.sunburst(
+    countryCityOrg, 
+    path=['organization_country', 'organization_city', 'organization_name'], 
+    values='prize',
+    title='Where do Discoveries Take Place?',
+)
+
+burst.update_layout(
+    xaxis_title='Number of Prizes', 
+    yaxis_title='City',
+    coloraxis_showscale=False
+)
+ 
+burst.show()
