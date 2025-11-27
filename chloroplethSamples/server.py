ee600b27ae34d7de504f454a4e568dd8acc5fcb1
Mars Cheung <mars.kwong.cheung@gmail.com>
2025-11-26 23:09:31 -0800

    add world wide chloropleth sample

--- chloroplethSamples/server.py
+++ chloroplethSamples/server.py
@@ -6,6 +6,8 @@ import plotly.graph_objs as go
 import plotly.io as pio 
 pio.renderers.default = "browser"
 
+# EXAMPLE OF BASIC USAGE
+
 # data = dict(type = 'choropleth',
 #             colorscale = 'Portland',
 #             locationmode = 'USA-states',
@@ -19,23 +21,45 @@ pio.renderers.default = "browser"
 # choromap = go.Figure(data = [data], layout=layout)
 # choromap.show()
 
-df = pd.read_csv('2011_US_AGRI_Exports')
+
+# UNITED STATES AGRICULTURAL EXPORTS
+
+# df = pd.read_csv('2011_US_AGRI_Exports')
+# data = dict(type = 'choropleth',
+#             colorscale = 'YlOrRd',
+#             locationmode = 'USA-states',
+#             locations = df['code'],
+#             text = df['text'],
+#             marker = dict(line = dict(color='rgb(255,255,255)', width=2)),
+#             z = df['total exports'],
+#             colorbar = {'title':'Millions USD'}
+# )
+
+# layout = dict(title='2011 US Agriculture Exports by State',
+#               geo=dict(
+#                   scope='usa', 
+#                   showlakes=True, 
+#                   lakecolor='rgb(85, 173, 240)'
+#                 )
+# )
+# choromap2 = go.Figure(data = [data], layout=layout)
+# choromap2.show()
+
+
+df = pd.read_csv('2014_World_GDP')
+
 data = dict(type = 'choropleth',
-            colorscale = 'YlOrRd',
-            locationmode = 'USA-states',
-            locations = df['code'],
-            text = df['text'],
-            marker = dict(line = dict(color='rgb(255,255,255)', width=2)),
-            z = df['total exports'],
-            colorbar = {'title':'Millions USD'}
+            locations = df['CODE'],
+            text = df['COUNTRY'],
+            z = df['GDP (BILLIONS)'],
+            colorbar = {'title':'GDP in Billions USD'}
 )
 
-layout = dict(title='2011 US Agriculture Exports by State',
+layout = dict(title='2014 Global GDP',
               geo=dict(
-                  scope='usa', 
-                  showlakes=True, 
-                  lakecolor='rgb(85, 173, 240)'
+                  showframe=False, 
+                  projection = {'type':'mercator'}
                 )
 )
-choromap2 = go.Figure(data = [data], layout=layout)
-choromap2.show()
+choromap3 = go.Figure(data = [data], layout=layout)
+choromap3.show()
